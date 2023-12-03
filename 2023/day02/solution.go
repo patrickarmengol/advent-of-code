992cbbc4a13e76ce80b538158bf3740dc83ced87
Patrick Armengol <patrickarmengol@protonmail.com>
2023-12-03 01:24:45 +0000

    redid solution for day02 with regex

--- 2023/day02/solution.go
+++ 2023/day02/solution.go
@@ -1,53 +1,102 @@
 package day02
 
 import (
-	"errors"
-	"fmt"
+	"regexp"
 	"slices"
 	"strconv"
-	"strings"
 
 	"github.com/patrickarmengol/advent-of-code/2023/helpers/parse"
 )
 
-// notes:
-// each set of cubes doesn't really matter, individual draws for a certain color do
-// so just get all draws for each color into a slice and iterate
-// this code avoids using regex for parsing, but would likely be easier with it
+// leaving old solution commented out here
 //
-// part 1:
-// iterate through games
-// if all draws are smaller than their color's max, add game num to total
+// func Part1(input string) (string, error) {
+// 	games := parse.Lines(input)
 //
-// part 2:
-// iterate through games
-// iterate through draws, updating "min" when larger value for a color is found
-// add the product of mins to the total
+// 	colorMaxes := map[string]int{
+// 		"red":   12,
+// 		"green": 13,
+// 		"blue":  14,
+// 	}
+//
+// 	total := 0
+// gameloop:
+// 	for i, game := range games {
+// 		draws, err := getAllDraws(game)
+// 		if err != nil {
+// 			return "", err
+// 		}
+// 		for _, draw := range draws {
+// 			cMax, ok := colorMaxes[draw.color]
+// 			if !ok {
+// 				return "", errors.New(fmt.Sprintf("draw color %v is not found in color max map", draw.color))
+// 			}
+// 			if draw.num > cMax {
+// 				continue gameloop
+// 			}
+// 		}
+// 		total += i + 1
+// 	}
+// 	return strconv.Itoa(total), nil
+// }
+//
+// func Part2(input string) (string, error) {
+// 	games := parse.Lines(input)
+//
+// 	total := 0
+// 	for _, game := range games {
+// 		colorMins := map[string]int{}
+// 		draws, _ := getAllDraws(game)
+// 		for _, draw := range draws {
+// 			if draw.num > colorMins[draw.color] {
+// 				colorMins[draw.color] = draw.num
+// 			}
+// 		}
+// 		total += colorMins["red"] * colorMins["green"] * colorMins["blue"]
+// 	}
+// 	return strconv.Itoa(total), nil
+// }
+//
+// type draw struct {
+// 	num   int
+// 	color string
+// }
+//
+// func getAllDraws(game string) ([]draw, error) {
+// 	draws := []draw{}
+// 	sets := strings.Split(strings.Split(game, ": ")[1], "; ")
+// 	for _, set := range sets {
+// 		ds := strings.Split(set, ", ")
+// 		for _, d := range ds {
+// 			drawParts := strings.Split(d, " ")
+// 			num, err := strconv.Atoi(drawParts[0])
+// 			if err != nil {
+// 				return nil, errors.New(fmt.Sprintln("problem parsing num for draw:", d))
+// 			}
+// 			color := drawParts[1]
+// 			if !slices.Contains([]string{"red", "green", "blue"}, color) {
+// 				return nil, errors.New(fmt.Sprintln("problem parsing color foor draw:", d))
+// 			}
+// 			draws = append(draws, draw{num: num, color: color})
+// 		}
+// 	}
+// 	return draws, nil
+// }
+
+var (
+	redRX   = regexp.MustCompile(`(\d+) red`)
+	greenRX = regexp.MustCompile(`(\d+) green`)
+	blueRX  = regexp.MustCompile(`(\d+) blue`)
+)
 
 func Part1(input string) (string, error) {
-	games := parse.Lines(input)
-
-	colorMaxes := map[string]int{
-		"red":   12,
-		"green": 13,
-		"blue":  14,
-	}
-
 	total := 0
-gameloop:
-	for i, game := range games {
-		draws, err := getAllDraws(game)
-		if err != nil {
-			return "", err
-		}
-		for _, draw := range draws {
-			cMax, ok := colorMaxes[draw.color]
-			if !ok {
-				return "", errors.New(fmt.Sprintf("draw color %v is not found in color max map", draw.color))
-			}
-			if draw.num > cMax {
-				continue gameloop
-			}
+	for i, game := range parse.Lines(input) {
+		maxR := slices.Max(numsFromMatches(redRX.FindAllStringSubmatch(game, -1)))
+		maxG := slices.Max(numsFromMatches(greenRX.FindAllStringSubmatch(game, -1)))
+		maxB := slices.Max(numsFromMatches(blueRX.FindAllStringSubmatch(game, -1)))
+		if maxR > 12 || maxG > 13 || maxB > 14 {
+			continue
 		}
 		total += i + 1
 	}
@@ -55,44 +104,24 @@ gameloop:
 }
 
 func Part2(input string) (string, error) {
-	games := parse.Lines(input)
-
 	total := 0
-	for _, game := range games {
-		colorMins := map[string]int{}
-		draws, _ := getAllDraws(game)
-		for _, draw := range draws {
-			if draw.num > colorMins[draw.color] {
-				colorMins[draw.color] = draw.num
-			}
-		}
-		total += colorMins["red"] * colorMins["green"] * colorMins["blue"]
+	for _, game := range parse.Lines(input) {
+		maxR := slices.Max(numsFromMatches(redRX.FindAllStringSubmatch(game, -1)))
+		maxG := slices.Max(numsFromMatches(greenRX.FindAllStringSubmatch(game, -1)))
+		maxB := slices.Max(numsFromMatches(blueRX.FindAllStringSubmatch(game, -1)))
+		total += maxR * maxG * maxB
 	}
 	return strconv.Itoa(total), nil
 }
 
-type draw struct {
-	num   int
-	color string
-}
-
-func getAllDraws(game string) ([]draw, error) {
-	draws := []draw{}
-	sets := strings.Split(strings.Split(game, ": ")[1], "; ")
-	for _, set := range sets {
-		ds := strings.Split(set, ", ")
-		for _, d := range ds {
-			drawParts := strings.Split(d, " ")
-			num, err := strconv.Atoi(drawParts[0])
-			if err != nil {
-				return nil, errors.New(fmt.Sprintln("problem parsing num for draw:", d))
-			}
-			color := drawParts[1]
-			if !slices.Contains([]string{"red", "green", "blue"}, color) {
-				return nil, errors.New(fmt.Sprintln("problem parsing color foor draw:", d))
-			}
-			draws = append(draws, draw{num: num, color: color})
+func numsFromMatches(matches [][]string) []int {
+	nums := []int{}
+	for _, m := range matches {
+		num, err := strconv.Atoi(m[1]) // capture group 1
+		if err != nil {
+			panic("problem with a regex definition")
 		}
+		nums = append(nums, num)
 	}
-	return draws, nil
+	return nums
 }
