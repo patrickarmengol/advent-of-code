dd0d214dc3522b1b0b730750fcb274b823aa42ef
Patrick Armengol <patrickarmengol@protonmail.com>
2023-12-04 14:12:17 +0000

    added a generic set

--- 2023/helpers/util/util.go
+++ 2023/helpers/util/util.go
@@ -1,5 +1,7 @@
 package util
 
+import "fmt"
+
 func Make2D[T any](n, m int) [][]T {
 	matrix := make([][]T, n)
 	rows := make([]T, n*m)
@@ -8,4 +10,57 @@ func Make2D[T any](n, m int) [][]T {
 		matrix[i] = rows[startRow:endRow:endRow]
 	}
 	return matrix
+}
+
+type Set[T comparable] map[T]struct{}
+
+func NewSet[T comparable](vals ...T) Set[T] {
+	s := Set[T]{}
+	for _, v := range vals {
+		s[v] = struct{}{}
+	}
+	return s
+}
+
+func (s Set[T]) Add(vals ...T) {
+	for _, v := range vals {
+		s[v] = struct{}{}
+	}
+}
+
+func (s Set[T]) Remove(val T) {
+	delete(s, val)
+}
+
+func (s Set[T]) Contains(val T) bool {
+	_, ok := s[val]
+	return ok
+}
+
+func (s Set[T]) Members() []T {
+	result := make([]T, 0, len(s))
+	for v := range s {
+		result = append(result, v)
+	}
+	return result
+}
+
+func (s Set[T]) String() string {
+	return fmt.Sprintf("%v", s.Members())
+}
+
+func (s Set[T]) Union(s2 Set[T]) Set[T] {
+	result := NewSet(s.Members()...)
+	result.Add(s2.Members()...)
+	return result
+}
+
+func (s Set[T]) Intersection(s2 Set[T]) Set[T] {
+	result := NewSet[T]()
+	for _, v := range s.Members() {
+		if s2.Contains(v) {
+			result.Add(v)
+		}
+	}
+	return result
 }
