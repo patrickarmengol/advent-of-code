892e2738b672aa2d5fe7376cd1e8642982b476de
Patrick Armengol <patrickarmengol@protonmail.com>
2023-12-15 15:45:18 +0000

    changed test fail output for easier yanking from term

--- 2023/helpers/assert/assert.go
+++ 2023/helpers/assert/assert.go
@@ -25,6 +25,6 @@ func Equal[T comparable](t *testing.T, got, want T) {
 	t.Helper()
 
 	if got != want {
-		t.Errorf("got: %v; want %v", got, want)
+		t.Errorf("got: %v ; want %v", got, want)
 	}
 }
